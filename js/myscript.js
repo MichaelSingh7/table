218be81cf70c742a818da9f717635ee0f93fa892
MichaelSingh <mikael.1@live.co.uk>
2019-09-12 17:29:57 +0000

    update table onto github

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("h2").addClass("underline");
+    $("ul").addClass("border");
+    $("tr:odd").css("background-color", "red");
+    $("tr:even").css("background-color", "orange");
+});
+
